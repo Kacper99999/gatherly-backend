41a5f78d71cd12b1cf0df1b195b5f3fcf4501922
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2025-06-11 20:32:16 +0200

    MeetupsRouter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import meetupRouter from './routes/meetupRouter.js';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use('/api/meetups', meetupRouter);
 
 app.get('/', (_req, res) => {
   res.send('Hello from Gatherly backend!');
